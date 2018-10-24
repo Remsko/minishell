93608c4db4a439bd2d4305e222e7d6f448f9f077
Remsko <pinoit.remi@gmail.com>
2018-10-24 13:17:45 +0200

    bonus

--- srcs/main.c
+++ srcs/main.c
@@ -6,48 +6,111 @@
 /*   By: rpinoit <rpinoit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/11 13:20:33 by rpinoit           #+#    #+#             */
-/*   Updated: 2018/10/23 14:16:52 by rpinoit          ###   ########.fr       */
+/*   Updated: 2018/10/24 13:16:42 by rpinoit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void    display_prompt(int error)
+void display_branch(int fd, char *buf)
 {
-    char cdw[256];
+    int ret;
+
+    if ((ret = read(fd, buf, 256)) > 5)
+    {
+        if (buf[ret - 1] == '\n')
+            buf[ret - 1] = '\0';
+        else
+            buf[ret] = '\0';
+        if (ft_strncmp(buf, "refs/", 5) == 0)
+        {
+            ft_putstr(" git:(");
+            (ft_strncmp(buf, "heads/", 6) != 0)
+                ? ft_putstr(buf + 11)
+                : ft_putstr(buf + 5);
+            ft_putstr(")");
+        }
+    }
+    close(fd);
+}
+
+void display_path(char *cwd)
+{
+    char *pos;
+
+    if (ft_strcmp(cwd + 1, env_search("HOME=")) == 0)
+    {
+        ft_putstr("~");
+        return;
+    }
+    if (ft_strrchr(ft_strchr(cwd, '/'), '/') == cwd)
+        pos = cwd;
+    else
+        pos = ft_strrchr(cwd, '/') + 1;
+    ft_putstr(pos);
+}
+
+void display_git(char *cwd)
+{
+    int fd;
+    char buf[256 + 1];
+    char *pos;
+
+    ft_strcat(cwd, "/");
+    pos = ft_strrchr(cwd, '/');
+    while (cwd + 1 < pos)
+    {
+        if ((pos = ft_strrchr(cwd, '/')) == NULL)
+            return;
+        *pos = '\0';
+        ft_strcat(pos, "/.git/HEAD");
+        if ((fd = open(cwd, O_RDONLY)) > 0)
+        {
+            if (read(fd, buf, 5) == 5)
+                return (display_branch(fd, buf));
+            close(fd);
+        }
+        *pos = '\0';
+    }
+}
+
+void display_prompt(int error)
+{
+    char cdw[256 + 1];
 
     if (error == 1)
-        write(1, "✗ ", 4);
+        write(1, "\u2717 ", 4);
     else
-        write(1, "✔ ", 4);
+        write(1, "\u2713 ", 4);
     if (getcwd(cdw, 256) != NULL)
-        ft_putstr(cdw);
+        display_path(cdw);
+    display_git(cdw);
     write(1, " $> ", 3);
 }
 
-void    read_cmdline(char **line, int *end)
+void read_cmdline(char **line, int *end)
 {
-    int     ret;
+    int ret;
 
     while ((ret = get_next_line(0, line)) != 1)
     {
         if (ret == -1)
         {
             print_error(ERROR_READ);
-            break ;
+            break;
         }
         else if (ret == 0)
         {
             *end = 1;
-            break ;
+            break;
         }
     }
 }
 
-char    *get_dollar_value(char *str)
+char *get_dollar_value(char *str)
 {
-    char    *val;
-    int     i;
+    char *val;
+    int i;
 
     i = 0;
     if (str == NULL || *str != '$')
@@ -63,14 +126,14 @@ char    *get_dollar_value(char *str)
     return (val);
 }
 
-void    env_rm(char *var, int var_len)
+void env_rm(char *var, int var_len)
 {
-    char    ***env;
-    char    **tmp_env;
-    char    **new_env;
-    int     len;
-    int     i;
-    int     j;
+    char ***env;
+    char **tmp_env;
+    char **new_env;
+    int len;
+    int i;
+    int j;
 
     env = &shell_singletone()->env;
     tmp_env = *env;
@@ -93,12 +156,12 @@ void    env_rm(char *var, int var_len)
     env = &new_env;
 }
 
-void    env_add(char *new_var)
+void env_add(char *new_var)
 {
-    char    ***env;
-    char    **tmp_env;
-    char    **new_env;
-    int     len;
+    char ***env;
+    char **tmp_env;
+    char **new_env;
+    int len;
 
     env = &shell_singletone()->env;
     tmp_env = *env;
@@ -112,13 +175,12 @@ void    env_add(char *new_var)
     env = &new_env;
 }
 
-
-char    *triple_join(char *begin, char *middle, char *end)
+char *triple_join(char *begin, char *middle, char *end)
 {
-    char    *join;
-    int     len1;
-    int     len2;
-    int     len3;
+    char *join;
+    int len1;
+    int len2;
+    int len3;
 
     len1 = ft_strlen(begin);
     len2 = ft_strlen(middle);
@@ -135,17 +197,17 @@ char    *triple_join(char *begin, char *middle, char *end)
     return (join);
 }
 
-char    *sandr_expansions(char *cmdline)
+char *sandr_expansions(char *cmdline)
 {
     ft_strdel(&cmdline);
     return (triple_join(ft_strdup("echo "), ft_strdup("/Users/rpinoit"), ft_strdup(" poulet")));
 }
 
-int     main(void)
+int main(void)
 {
-	extern char **environ;
-    t_shell     *shell;
-    char        *line;
+    extern char **environ;
+    t_shell *shell;
+    char *line;
 
     shell = shell_singletone();
     shell->env = env_copy(environ);
@@ -155,7 +217,7 @@ int     main(void)
         shell->error = 0;
         read_cmdline(&line, &shell->end);
         if (shell->end == 1)
-            break ;
+            break;
         shell->cmdline = line;
         execute_cmdline(shell->cmdline);
         shell_clear(shell);
